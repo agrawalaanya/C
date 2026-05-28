733d409a96583e88aa65c8553d1658fd42596f23
Aanya Agrawal <aanyaagrawal834@gmail.com>
2026-05-28 23:27:38 +0530

    spiral and Z pattern

---
+++ lect17/spiralPtrn.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    for(int i=0;i<1;i++){
+        for(int j=0;j<3;j++){
+            printf("%d",arr[i][j]);
+        }
+    }
+    for(int i=1;i<3;i++){
+        for(int j=2;j<3;j++){
+            printf("%d",arr[i][j]);
+        }
+    }
+    for(int i=2;i<3;i++){
+        for(int j=1;j>=0;j--){
+            printf("%d",arr[i][j]);
+        }
+    }    
+    for(int i=1;i<2;i++){
+        for(int j=0;j<2;j++){
+            printf("%d",arr[i][j]);
+        }
+    }
+}
